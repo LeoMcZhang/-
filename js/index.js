8ec906d3e8c199028f079396f6753153e3abbf81
ZhangYX <you@example.com>
2017-02-25 01:20:35 +0800

    123s

---
+++ js/index.js
@@ -0,0 +1,25 @@
+      
+(function (doc, win) {    
+    var docEl = doc.documentElement,    
+    resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',    
+    recalc = function () {    
+	    var clientWidth = docEl.clientWidth;    
+	    if (!clientWidth) return; 
+	    if(clientWidth>=740){	  
+	    	console.log(1)
+		    docEl.style.fontSize = 100 + 'px';   		    
+	    }
+	    if(clientWidth<740&&clientWidth>320){
+	    	docEl.style.fontSize = 100*clientWidth/740 + 'px';   
+	    }
+	    if(clientWidth<=320){	    	
+		    docEl.style.fontSize = 100*320/740 + 'px';   		    
+	    }
+	};    
+	if (!doc.addEventListener) return;    
+	win.addEventListener(resizeEvt, recalc, false);    
+	doc.addEventListener('DOMContentLoaded', recalc, false); 
+	console.log(docEl.clientWidth);
+})(document, window); 
+
+
