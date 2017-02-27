26a7c9cdff1c0b47c5c36e0fa5b4857353c5a402
ZhangYX <you@example.com>
2017-02-28 01:09:07 +0800

    搜索页历史记录JS

---
+++ js/search.js
@@ -0,0 +1,54 @@
+
+
+new Vue({
+	el:"#box",
+	data:{
+		history:[],
+		text:''
+	},
+	methods:{
+		subInfo(){
+			var str = localStorage.getItem('history');
+			var arr = str==''?[]:JSON.parse(str);
+			var text = this.text;
+			if(text == ''){
+				return;
+			}
+			arr.push(text);
+			var str = JSON.stringify(arr);
+			localStorage.setItem('history',str);	
+			this.history = arr ;
+			this.text ='';	
+		},
+		clearRecord(){
+			
+			localStorage.setItem('history','');
+
+			this.history = [];
+		}
+	},
+	//钩子函数，DOM加载完成
+	created(){
+
+		// var history = localStorage.getItem('history');
+
+		// var arr = history=='' ? []:JSON.parse(history);
+		
+		// this.history = arr ;
+
+		// alert(history=='');	
+		var str = localStorage.getItem('history');
+		var arr = str==''?[]:JSON.parse(str);
+		this.history = arr ;
+	}
+})
+
+
+
+
+
+
+
+
+
+
